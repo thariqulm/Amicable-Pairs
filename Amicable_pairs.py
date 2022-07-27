c159620e581d6de2484fa2b674a2822fa07ef55e
Thariq <63181824+thariqulm@users.noreply.github.com>
2022-07-27 20:00:32 +0530

    Update Amicable_pairs.py

--- Amicable_pairs.py
+++ Amicable_pairs.py
@@ -1,33 +1,48 @@
 import time
 from sympy.ntheory.factor_ import divisor_sigma
-from itertools import count
-initial = int(input("Enter two numbers p & q between which Amicable pairs need to be generated\n p = "))
+
+initial = int(
+    input(
+        "Enter two numbers p & q between which Amicable pairs need to be generated\n p = "
+    ))
 final = int(input("\n q = "))
-print("\n")
+d = 0
+div_u = 0
+print(" ")
 st = time.time()
+
+def v_temp(h):
+    global d, div_u
+    div_u = divisor_sigma(h)
+    d = div_u - h
+    return d
+
+
 def main():
-  k=0
-  j=1
-  ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
-  for u in count(initial):
-    div_u=divisor_sigma(u)
-    v=div_u-u
-    if v>u and div_u==divisor_sigma(v):
-      if initial<=220:
-        print(ordinal(j) + " Amicable pair: (%s,%s)"%(u,v))
-        k=1
-        j=j+1
-      else:
-        print("(%s,%s) are amicable numbers "%(u,v))
-        k=1
-    if u>final:
-      break;
-  if k==0:
-    print("There are no Amicable Numbers in the given range")
-  else:
-    print("The above numbers are the Amicable pairs generated between %s and %s"%(initial,final))
-  et = time.time()
-  print('\nExecution time:', et-st, 'seconds')
+    global d, div_u
+    ordinal = lambda n: "%d%s" % (n, "tsnrhtdd"[(n // 10 % 10 != 1) *
+                                                (n % 10 < 4) * n % 10::4])
+    Amicable = list((w, d) for w in range(initial, final + 1)
+                    if v_temp(w) > w and div_u == divisor_sigma(d))
+    if len(Amicable) == 0:
+        print("There are no Amicable Numbers in the given range")
+    elif 220 in Amicable[0]:
+        k = 1
+        for i in iter(Amicable):
+            print(ordinal(k) + " Amicable pair: " + str(i))
+            k = k + 1
+        print(
+            "The above numbers are the Amicable pairs generated between %s and %s"
+            % (initial, final))
+    elif 220 not in Amicable[0]:
+        for i in iter(Amicable):
+            print(" Amicable pair: " + str(i))
+        print(
+            "The above numbers are the Amicable pairs generated between %s and %s"
+            % (initial, final))
+    et = time.time()
+    print('\nExecution time:', et - st, 'seconds')
+
 
 if __name__ == "__main__":
     main()
